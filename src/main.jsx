c98fdfc3c4af8f7d2f156d9ee3fbffebdb841690
shuklashobhit0001-droid <shobhitshukla@example.com>
2025-11-29 14:34:46 +0530

    Fix: Revert ElevenLabs provider - useConversation works standalone

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ElevenLabsConversationProvider } from '@elevenlabs/react'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ElevenLabsConversationProvider>
-      <App />
-    </ElevenLabsConversationProvider>
+    <App />
   </StrictMode>,
 )
